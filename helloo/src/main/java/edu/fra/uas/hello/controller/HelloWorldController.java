fab48b3ad1ed8514f810cfd5c8f33740a640ba30
yunusAktas2004 <yunus.aktas@stud.fra-uas.de>
2025-11-03 17:11:12 +0100

    yusuf prime

---
+++ helloo/src/main/java/edu/fra/uas/hello/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package edu.fra.uas.hello.controller;
+
+import org. springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMethod; 
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class HelloWorldController {
+    @RequestMapping(value="hello", method=RequestMethod.GET)
+@ResponseBody
+
+public String gethelloworld(){
+    return "<h1> Yusuf prime</h1>";
+}
+
+}
